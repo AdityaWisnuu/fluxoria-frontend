31cc95e33a003f4a4b20ed0da1cb42642eab71f2
AdityaWisnuu <adtywisnuwardana003@gmail.com>
2025-10-15 03:04:39 +0700

    fix: try add dynamic renderin in spot page

--- src/app/spot/[id]/page.tsx
+++ src/app/spot/[id]/page.tsx
@@ -1,10 +1,21 @@
-import SpotPage from "@/features/spot/components/SpotPage";
+"use client";
+
+import dynamic from "next/dynamic";
 
 interface SpotPageProps {
   params: Promise<{
     id: string;
   }>;
 }
+
+const SpotPage = dynamic(() => import("@/features/spot/components/SpotPage"), {
+  ssr: false,
+  loading: () => (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
+    </div>
+  ),
+});
 
 export default async function Spot({ params }: SpotPageProps) {
   const { id } = await params;
